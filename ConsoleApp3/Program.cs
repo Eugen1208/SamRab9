9c0e83a4d2ec815b3ac36b8c672accaa86caec40
User <User@E-NOTE>
2022-11-07 22:27:46 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вас приветствует калькулятор!");
+            Console.WriteLine("Введите целое число X:");
+
+            try
+            {
+                X = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите код операции:");
+            code = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число Y:");
+            Y = Convert.ToDouble(Console.ReadLine());
+                double X = Convert.ToDouble(Console.ReadLine());
+                double Y = Convert.ToDouble(Console.ReadLine());
+                double r = X / Y;
+                Console.WriteLine(r);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double X;
+            double Y;
+            double total;
+            char code;
+            Console.WriteLine("Вас приветствует калькулятор!");
+            Console.Write("Введите целое число X:");
+            try
+            {
+                X = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите целое число Y:");
+                Y = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите код операции:");
+                Console.WriteLine("1 - сложение");
+                Console.WriteLine("2 - вычитание");
+                Console.WriteLine("3 - умножение");
+                Console.WriteLine("4 - деление");
+                Console.Write("Ваш выбор: ");
+                code = Convert.ToChar(Console.ReadLine());
+                if (code == '1')
+                {
+                    total = X + Y;
+                    Console.WriteLine("Результат = " + total + ".");
+                }
+
+                else if (code == '2')
+                {
+                    total = X - Y;
+                    Console.WriteLine("Результат = " + total + ".");
+                }
+
+                else if (code == '3')
+                {
+                    total = X * Y;
+                    Console.WriteLine("Результат = " + total + ".");
+                }
+
+                else if (code == '4')
+                {
+                    total = X / Y;
+                    Console.WriteLine("Результат = " + total + ".");
+                }
+                else
+                {
+                    Console.WriteLine("Нет операции с указанным номером");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
